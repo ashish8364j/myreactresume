0ba076e76cc3994391fcd1500bdcd7234b1ebed7
Ashish Singh <ashishsingh822003@gmail.com>
2024-01-27 00:46:22 +0530

    newww

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-
-export default {
-  build: {
-    outDir: 'dist', // Specify the correct output directory
-  },
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react()],
+})
